29457ea2ac3852762ec34a1d929bdc7945f954f3
MuhammadHatta72 <attah1335@gmail.com>
2026-01-16 11:31:51 +0700

    feat: Update filesystem configuration to add 'assets' disk and improve storage access in Nginx

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,16 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL'), '/').'/storage',
+            'url' => rtrim(env('APP_URL'), '/') . '/storage',
+            'visibility' => 'public',
+            'throw' => false,
+            'report' => false,
+        ],
+
+        'assets' => [
+            'driver' => 'local',
+            'root' => public_path(),
+            'url' => rtrim(env('APP_URL'), '/'),
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
