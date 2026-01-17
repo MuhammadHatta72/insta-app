468505e1e60dee0dff96e58da6bf4db9e2c127c5
MuhammadHatta72 <attah1335@gmail.com>
2026-01-17 07:24:56 +0700

    refactor: Update destroy method in PostController to include Request parameter for improved authorization handling

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -270,7 +270,7 @@ class PostController extends Controller
         return redirect()->route('posts.index');
     }
 
-    public function destroy(Post $post)
+    public function destroy(Request $request, Post $post)
     {
         if (auth()->id() !== $post->user_id) {
             abort(403, 'Unauthorized');
